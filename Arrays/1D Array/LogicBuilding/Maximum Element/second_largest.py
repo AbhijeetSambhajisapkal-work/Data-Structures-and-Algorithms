6b9c74dd487d5835c354acc4cd320e474dabf8e4
AbhijeetSambhajisapkal-work <Abhijeet.Sapkal.work@gmail.com>
2025-06-05 02:45:45 +0530

    added second_largestnumberlogic

---
+++ Arrays/1D Array/LogicBuilding/Maximum Element/second_largest.py
@@ -0,0 +1,19 @@
+def second_largest(A):
+    largest_number = A[0];
+    largest_index = 0;
+    for x in range(1,len(A)):
+        if(A[x] > largest_number):
+            largest_number = A[x];
+            largest_index = x;
+
+    second_largest = A[0];
+    for x in range(0,len(A)):
+        if(x != largest_index and A[x] != largest_number):
+            if(A[x] > second_largest):
+                second_largest= A[x];
+
+    print(second_largest);
+    
+if __name__ == "__main__":
+    A = [1, 4, 4, 5, 5]
+    second_largest(A) # 1,[1,2], [1,2,3],[1,2,3,4]......
